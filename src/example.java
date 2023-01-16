9b0bc4336b3c3573eb3fd2ba8ced8408cd9df5bd
k_dongja@encs.concordia.ca <k_dongja@encs.concordia.ca>
2023-01-16 09:14:14 -0500

    first commit

---
+++ src/example.java
@@ -0,0 +1,14 @@
+
+public class example {
+	public static void main (String [] args) {
+		int x = 0;
+		if (x == 0) {
+			
+		}
+		try {
+			System.out.println("hello");
+		}catch(Exception e ) {
+			
+		}
+	}
+}
