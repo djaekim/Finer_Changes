3aa4893473a2e336612b3967c8076532d7a34941
k_dongja@encs.concordia.ca <k_dongja@encs.concordia.ca>
2023-01-16 21:18:45 -0500

    add new code

--- src/example.java
+++ src/example.java
@@ -3,7 +3,10 @@ public class example {
 	public static void main (String [] args) {
 		int x = 0;
 		if (x == 0) {
-			
+			System.out.println("if statement");
+		}
+		else {
+			System.out.println("else statement");
 		}
 		try {
 			System.out.println("hello");
